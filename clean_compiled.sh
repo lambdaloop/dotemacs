2060bf9a73df9d1f28746b84fe033b2d1b5e6c9a
Pierre Karashchuk <krchtchk@gmail.com>
2021-07-06 16:45:36 -0700

    more files

---
+++ clean_compiled.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# remove files that cause trouble
+rm ~/.emacs.d/.local/straight/build/jupyter/*.elc
+rm ~/.emacs.d/.local/straight/build/vterm-toggle/vterm-toggle.elc
---
+++ custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
+ '(org-agenda-files
+   '("/jellyfish/notes/org/todo/ideas.org" "~/notes/org/todo/anipose.org" "~/notes/org/todo/writing.org" "~/notes/org/todo/clubs.org" "~/notes/org/todo/learning.org" "~/notes/org/todo/setup.org" "~/notes/org/todo/fly_kinematics.org" "~/notes/org/todo/scheduling.org" "~/notes/org/todo/personal.org" "~/notes/org/todo/evodev.org" "~/notes/org/todo/inbox.org"))
+ '(safe-local-variable-values '((org-image-actual-width . 300))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(vterm-color-black ((t (:background "darkgray" :foreground "black")))))
+(put 'narrow-to-region 'disabled nil)
+(put 'narrow-to-page 'disabled nil)
